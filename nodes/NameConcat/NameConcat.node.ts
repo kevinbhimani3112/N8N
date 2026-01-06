c2fc31291086a0075af427bc5b0d45bf698f9c30
Yash <yash.madariya@esparkbizmail.com>
2026-01-06 14:06:39 +0530

    nameconcat node implementation

---
+++ nodes/NameConcat/NameConcat.node.ts
@@ -0,0 +1,59 @@
+import {
+    IExecuteFunctions,
+    INodeExecutionData,
+    INodeType,
+    INodeTypeDescription,
+} from 'n8n-workflow';
+
+export class NameConcat implements INodeType {
+    description: INodeTypeDescription = {
+        displayName: 'Name Concatenator',
+        name: 'nameConcat',
+        icon: 'file:nameConcat.svg',
+        group: ['transform'],
+        version: 1,
+        description: 'Concatenates first and last name',
+        defaults: {
+            name: 'Name Concatenator',
+        },
+        inputs: ['main'],
+        outputs: ['main'],
+        properties: [
+            {
+                displayName: 'First Name',
+                name: 'firstName',
+                type: 'string',
+                default: '',
+                placeholder: 'John',
+                description: 'The first name',
+            },
+            {
+                displayName: 'Last Name',
+                name: 'lastName',
+                type: 'string',
+                default: '',
+                placeholder: 'Doe',
+                description: 'The last name',
+            },
+        ],
+    };
+
+    async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+        const items = this.getInputData();
+        const returnData: INodeExecutionData[] = [];
+
+        for (let i = 0; i < items.length; i++) {
+            const firstName = this.getNodeParameter('firstName', i) as string;
+            const lastName = this.getNodeParameter('lastName', i) as string;
+            const fullName = `${firstName} ${lastName}`;
+
+            returnData.push({
+                json: {
+                    fullName,
+                },
+            });
+        }
+
+        return [returnData];
+    }
+}
